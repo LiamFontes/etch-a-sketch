bd5fe84b2cd6ffd2e1c5eab50a4ea2c6665c9169
LiamFontes <liamfontes1244@gmail.com>
2021-06-29 21:38:36 -0400

    basic sketch functionality completed. Still to add different color options and web layout

--- script.js
+++ script.js
@@ -1,45 +1,77 @@
 const sliderValue = document.getElementById("range");
 const sliderValueContainer = document.getElementById("sliderValueDiv");
 
+function colorChanger() {
+    const divs = document.querySelectorAll('.divRepeat');
+
+    divs.forEach(item => {
+        item.addEventListener('mouseenter', colorChange => {
+            item.style.backgroundColor = "red";
+        })
+    })
+}
+
 
 sliderValueContainer.innerText = sliderValue.value;
 
+window.addEventListener('load', placeBoardDivs(32));
+colorChanger();
+
+
+
 
 //Update the value of the slider
-
 //Update the Number of Divs in the board
-sliderValue.oninput = function boardDiveUpdate() {
+sliderValue.oninput = function boardDivUpdate() {
 
     sliderValueContainer.innerText = this.value;
     clearBoard();
 
-    numOfDivs = this.value;
+    numOfDivs = Number(this.value);
+    placeBoardDivs(numOfDivs)
+    colorChanger();
+}
+
+function placeBoardDivs(numOfDivs) {
     const boardContainer = document.getElementById("boardContainer");
-    
-    for (i = 0; i <= numOfDivs;i++){
-        let div = document.createElement('div');
-        
-        div.id = `div{$i}`;
-        div.className = "divRepeat";
-        div.innerText = "1"
-        boardContainer.appendChild(div);
+    const containerWidth = boardContainer.offsetWidth;
+    const divWidth = containerWidth / (numOfDivs + 1);
+
+    for (j = 0; j < numOfDivs; j++) {
+
+        for (i = 0; i < numOfDivs; i++) {
+
+            let div = document.createElement('div');
+            div.id = "div" + String(i);
+            div.className = "divRepeat";
+            div.style.width = String(divWidth) + "px";
+            div.style.flexGrow = "1";
+
+            boardContainer.appendChild(div);
+        }
     }
-   
-   }
+}
 
 
 
-   function clearBoard(){
+function clearBoardButton() {
+    const divs = document.querySelectorAll('.divRepeat');
+
+    divs.forEach(item => {
+        item.style.backgroundColor = "white";
+    })
+}
+
+function clearBoard() {
     const boardContainer = document.getElementById("boardContainer")
     boardContainer.innerText = "";
 
-   }
+}
 
 /*TO DO:
 
 Must add the following features:
-   - Function: Calculate the div size based on the size of the board
-   - Function: add event handler for hovering
    - add buttons/selections for different coloring
    - clear board button
-   
+
+*/
